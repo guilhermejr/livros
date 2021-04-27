d798e72db8271033137832b3fa5da13e2f2ac926
Guilherme Jr <falecom@guilhermejr.net>
2021-04-27 09:23:58 -0300

    feat: verifica se o token jwt está expirado

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,32 @@
+import jwt_decode from 'jwt-decode';
+
 export const TOKEN = 'livros-token';
 
-export const isAutenticado = () => localStorage.getItem(TOKEN) !== null;
+export const isAutenticado = () => {
+    
+    const token = localStorage.getItem(TOKEN);
+
+    if (token === null) {
+        return false;
+    } else {
+        const jwt = jwt_decode(token);
+        
+        const agora = new Date();
+        const tokenExpiracao = jwt.exp * 1000;
+
+        if (agora.getTime() > tokenExpiracao) {
+
+            logout();
+
+            const dateObject = new Date(tokenExpiracao);
+            console.log("Expirou em: " + dateObject.toLocaleString());
+            
+            return false;
+        }
+
+        return true;
+    }
+}
 
 export const getToken = () => localStorage.getItem(TOKEN);
 
